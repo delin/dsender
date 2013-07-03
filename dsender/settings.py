037c0a37f8962a4727899e61906b8ae2bffc4e15
Yulin Denis <delin@delin.pro>
2013-07-04 02:22:41 +0400

    почти…

--- dsender/settings.py
+++ dsender/settings.py
@@ -153,3 +153,8 @@ LOGGING = {
         },
     }
 }
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '465'
+EMAIL_USE_TLS = True
--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
+from dsender import settings
 from main.models import Project, Mailing
 
 
@@ -19,3 +23,22 @@ class StartMailing(TemplateView):
         context['mailing_lists'] = Mailing.objects.all()[:10]
         context['title'] = "Start Mailing"
         return context
+
+    def post(self, request, *args, **kwargs):
+        if not request.POST['mailing_list']:
+            return redirect('start_mailing')
+
+        mailing_list = request.POST['mailing_list']
+
+        mailing = Mailing.objects.get(id=mailing_list)
+        project = mailing.project
+        email_list = project.email_list.all()
+
+        settings.EMAIL_HOST = project.from_account.smtp_server
+        for email in email_list:
+            print("Sended to ", email.email)
+            send_mail(subject=mailing.subject, message=mailing.text, from_email=project.from_account.smtp_user,
+                      recipient_list=[email.email], fail_silently=False,
+                      auth_user=project.from_account.smtp_user, auth_password=project.from_account.smtp_password)
+
+        return redirect('start_mailing')
